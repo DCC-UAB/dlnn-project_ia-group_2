2e50a2acce9dc41f4a78cf159415ad3023d6cc65
Miguel Moral <miguelmoralhernandez@gmail.com>
2023-05-18 23:35:52 +0200

    changed size of val loader

--- get_loader_v2_train_val_test.py
+++ get_loader_v2_train_val_test.py
@@ -102,7 +102,15 @@ class ImageCaptionDataset(Dataset):
         return padded_caption
 
 
-def get_loader(data_dir, dataframe, transform=None, batch_size=16, num_workers=1, shuffle=True, pin_memory=True):
+def get_train_loader(data_dir, dataframe, transform=None, batch_size=16, num_workers=1, shuffle=True, pin_memory=True):
+    dataset = ImageCaptionDataset(data_dir=data_dir, dataframe=dataframe, transform=transform)
+    pad_idx = dataset.vocab.stoi['<PAD>']
+    data_loader  = DataLoader(dataset=dataset, batch_size=batch_size,
+                         num_workers=num_workers, shuffle=shuffle,
+                         pin_memory=pin_memory) 
+    return data_loader 
+
+def get_val_test_loader(data_dir, dataframe, transform=None, batch_size=4, num_workers=1, shuffle=True, pin_memory=True):
     dataset = ImageCaptionDataset(data_dir=data_dir, dataframe=dataframe, transform=transform)
     pad_idx = dataset.vocab.stoi['<PAD>']
     data_loader  = DataLoader(dataset=dataset, batch_size=batch_size,
