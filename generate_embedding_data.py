b3cbea2e01eaf49b9d2b2877dcaba861087d52da
pareva <polmedinaarevalo@gmail.com>
2023-05-27 19:10:47 +0200

    Updated scripts for pretrained embedding 300 dim

--- generate_embedding_data.py
+++ generate_embedding_data.py
@@ -3,15 +3,17 @@ import pickle
 import numpy as np
 
 main_path_miguel = r'C:\Users\Miguel\OneDrive\Escritorio\dlnn-project_ia-group_2\GLOVE EMBEDDING\glove_files'
+main_path_pol = r"C:\Users\polme\OneDrive - UAB\Escritorio\Universitat\Deep Learning\Embeddings"
 
-glove_path = main_path_miguel
+
+glove_path = main_path_pol
 
 words = []
 idx = 0
 word2idx = {}
-vectors = bcolz.carray(np.zeros(1), rootdir=f'{glove_path}/6B.100.dat', mode='w')
+vectors = bcolz.carray(np.zeros(1), rootdir=f'{glove_path}/6B.300.dat', mode='w')
 
-with open(f'{glove_path}/glove.6B.100d.txt', 'rb') as f:
+with open(f'{glove_path}/glove.6B.300d.txt', 'rb') as f:
     for l in f:
         line = l.decode().split()
         word = line[0]
@@ -21,7 +23,7 @@ with open(f'{glove_path}/glove.6B.100d.txt', 'rb') as f:
         vect = np.array(line[1:]).astype(float)
         vectors.append(vect)
     
-vectors = bcolz.carray(vectors[1:].reshape((400000, 100)), rootdir=f'{glove_path}/6B.100.dat', mode='w')
+vectors = bcolz.carray(vectors[1:].reshape((400000, 300)), rootdir=f'{glove_path}/6B.300.dat', mode='w')
 vectors.flush()
-pickle.dump(words, open(f'{glove_path}/6B.100_words.pkl', 'wb'))
-pickle.dump(word2idx, open(f'{glove_path}/6B.100_idx.pkl', 'wb'))
+pickle.dump(words, open(f'{glove_path}/6B.300_words.pkl', 'wb'))
+pickle.dump(word2idx, open(f'{glove_path}/6B.300_idx.pkl', 'wb'))
