c470b2e3bcd84ead313b906be6254fc385fca421
pareva <polmedinaarevalo@gmail.com>
2023-05-27 18:47:26 +0200

    Update script for generating 300 dim embedding

---
+++ generate_embedding_data.py
@@ -0,0 +1,27 @@
+import bcolz
+import pickle
+import numpy as np
+
+main_path_miguel = r'C:\Users\Miguel\OneDrive\Escritorio\dlnn-project_ia-group_2\GLOVE EMBEDDING\glove_files'
+
+glove_path = main_path_miguel
+
+words = []
+idx = 0
+word2idx = {}
+vectors = bcolz.carray(np.zeros(1), rootdir=f'{glove_path}/6B.100.dat', mode='w')
+
+with open(f'{glove_path}/glove.6B.100d.txt', 'rb') as f:
+    for l in f:
+        line = l.decode().split()
+        word = line[0]
+        words.append(word)
+        word2idx[word] = idx
+        idx += 1
+        vect = np.array(line[1:]).astype(float)
+        vectors.append(vect)
+    
+vectors = bcolz.carray(vectors[1:].reshape((400000, 100)), rootdir=f'{glove_path}/6B.100.dat', mode='w')
+vectors.flush()
+pickle.dump(words, open(f'{glove_path}/6B.100_words.pkl', 'wb'))
+pickle.dump(word2idx, open(f'{glove_path}/6B.100_idx.pkl', 'wb'))
